1f69b7e5131a023ae75e89861d6766a1148a7929
vi_mi <fenil.jain2018@vitstudent.ac.in>
2020-01-16 18:48:48 +0530

    Removing node_modules

--- index.js
+++ index.js
@@ -33,12 +33,12 @@ app.use(cors());
 const db = knex({
     client: 'pg',
     connection: {
-        // host: '127.0.0.1',
-        // user: 'postgres',
-        // password: '1234',
-        // database: 'onefourthlabstask'
-        connectionString: process.env.DATABASE_URL,
-        ssl: true
+        host: '127.0.0.1',
+        user: 'postgres',
+        password: '1234',
+        database: 'onefourthlabstask'
+        // connectionString: process.env.DATABASE_URL,
+        // ssl: true
     }
 });
 
